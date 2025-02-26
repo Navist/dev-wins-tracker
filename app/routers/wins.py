d2315235e2374651e4ca701612393a8c5348ed0c
Navist <zachstheman2@hotmail.com>
2025-02-26 11:11:03 -0500

    CRUD additions, still needs update functionality and testing.

--- app/routers/wins.py
+++ app/routers/wins.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.checks import check_admin, check_db_user, get_db
 from sqlalchemy.orm import Session
-from app.schemas import WinResponse, CustomCategoryCreate, WinCreate
-from app.models import User, Win, CustomCategory
+from app.schemas import WinResponse, WinCreate, WinDelete, WinDeleteResponse
+from app.models import Win, CustomCategory, PredefinedCategory
 from app.security import verify_token
 
 router = APIRouter(
@@ -19,21 +19,23 @@ router = APIRouter(
 #     db_user = check_db_user(db, token_data)
 
 
-@router.post("/", response_model=WinResponse)
+@router.post("/create/", response_model=WinCreate)
 def create_win(win: WinCreate, db: Session = Depends(get_db), token_data: dict = Depends(verify_token)):
     # Should check to see if user already has a category description so we don't create duplicates
+    # This will be called to create a new win when the user wants to add one but the categories will be pulled from both predefined sql table and custom categories.
+    # An option on the drop down menu will include "Create New Category" or similar for creating custom.
+    # Current method of posting doesn't require that the category already exist inside the predefined or custom tables but will be added later.
 
-    # db_win_exists = db.query(Win).filter(win.category_name == )
+    check_predef_exists = db.query(PredefinedCategory).filter(PredefinedCategory.name == win.category).first()
+    check_custom_exists = db.query(CustomCategory).filter(CustomCategory.name == win.category).first()
 
-    # if db_win_exists:
-    #     raise HTTPException(status_code=409, detail="Category description already exists")
+    if not check_predef_exists and not check_custom_exists:
+        raise HTTPException(status_code=404, detail="Category not found")
 
     db_win = Win(
         user_id=token_data["user_id"],
-        category_name=win.category_name,
-        description=win.description,
-        category_id=win.category_id,
-        category_type=win.category_type
+        category=win.category,
+        description=win.description
     )
     db.add(db_win)
     db.commit()
@@ -41,14 +43,50 @@ def create_win(win: WinCreate, db: Session = Depends(get_db), token_data: dict =
     return db_win
 
 
+@router.get("/read/all", response_model=list[WinResponse])
+def read_wins(db: Session = Depends(get_db), token_data: dict=Depends(verify_token)):
+    user_wins = db.query(Win).filter(Win.user_id == token_data['user_id']).all()
 
-def read_win():
+    if len(user_wins) <= 0:
+        raise HTTPException(status_code=404, detail="Create some memories!")
+
+    return user_wins
+
+
+@router.get("/read/filtered", response_model=list[WinResponse])
+def read_win(win: WinResponse, db: Session = Depends(get_db), token_data: dict=Depends(verify_token)):
+    Win_table = db.query(Win)
+    user_wins = Win_table.filter(Win.user_id == token_data['user_id'])
+    specific_category = user_wins.filter(Win.category.ilike(f"%{win.category}%")).all()
+
+
+    if len(specific_category) <= 0:
+        raise HTTPException(status_code=404, detail="Create some memories!")
+
+    return specific_category
+
+@router.post("/update/")
+def update_win(db: Session = Depends(get_db), token_data: dict=Depends(verify_token)):
     pass
 
-def update_win():
-    pass
+@router.delete("/delete/", response_model=WinDeleteResponse)
+def delete_win(win: WinDelete, db: Session = Depends(get_db), token_data: dict=Depends(verify_token)):
+    
+    user_wins = db.query(Win).filter(Win.user_id == token_data['user_id'])
 
+    specific_win = user_wins.filter_by(id=win.id).first()
 
-def delete_win():
-    pass
+    if not specific_win:
+        raise HTTPException(status_code=404, detail="No wins with that id found.")
+
+    response_data = {
+        "category": specific_win.category,
+        "description": specific_win.description,
+        "reason": "deleted"
+    }
+
+    db.delete(specific_win)
+    db.commit()
+
+    return response_data
 
