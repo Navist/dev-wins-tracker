8306b2176a91bf8959527af8b3930e99676ca4a2
Navist <zachstheman2@hotmail.com>
2025-03-02 04:17:20 -0500

    Reoganized routers to be alphabetical and added our new router, subscribers.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from app.routers import users, wins, categories
+from app.routers import users, wins, categories, subscribers
 
 app = FastAPI()
 
 
 app.include_router(users.router)
-app.include_router(wins.router)
 app.include_router(categories.router)
+app.include_router(subscribers.router)
+app.include_router(wins.router)
+
 
 
 # Displays how it's possible to add FastAPI functions inside the main py but it's easier and more modular to do it inside their own router files.
