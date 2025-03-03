1612d2fa3b058bb26c15f31c3fdc17421473ca9e
Navist <zachstheman2@hotmail.com>
2025-03-03 09:57:21 -0500

    This currently isn't using but to avoid 'use client' on all landing pages we'll be creating individual buttons like this to reduce client loads and load from server instead.

---
+++ dev-wins-frontend/src/app/components/LoginButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const LoginButton = () => {
+    return (
+        <div>
+            <button onClick={() => console.log("Login Button Clicked")}>
+                Login
+            </button>
+        </div>
+    );
+};
+
+export default LoginButton;
