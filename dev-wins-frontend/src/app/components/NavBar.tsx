63f558838bf9db1508a54f11618e9b4bcedc8ad3
Navist <zachstheman2@hotmail.com>
2025-03-04 13:30:59 -0500

    Fixed typo

--- dev-wins-frontend/src/app/components/NavBar.tsx
+++ dev-wins-frontend/src/app/components/NavBar.tsx
@@ -18,7 +18,7 @@ export default function NavBar() {
                 onClick={() => router.push("/categories")}
                 className="px-4 py-2 bg-green-500 rounded"
             >
-                Manage Cateogories
+                Manage Categories
             </button>
             <button
                 onClick={() => router.push("/subscriptions")}
