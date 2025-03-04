cc2a7fa65fb8b0a7ad2f6b90a30caee7812e6289
Navist <zachstheman2@hotmail.com>
2025-03-04 13:32:01 -0500

    Rendering of cards for Wins. Added onClick buttons.

--- dev-wins-frontend/src/app/components/WinCard.tsx
+++ dev-wins-frontend/src/app/components/WinCard.tsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-interface WinProps {
+interface Win {
     id: number;
+    title: string;
     category: string;
     description: string;
 }
 
-export default function WinCard({ id, category, description }: WinProps) {
+interface WinProps {
+    id: number;
+    title: string;
+    category: string;
+    description: string;
+    onDelete: (id: number) => Promise<void>;
+    onEdit: (win: Win) => void;
+}
+
+export default function WinCard({
+    id,
+    title,
+    category,
+    description,
+    onDelete,
+    onEdit,
+}: WinProps) {
     return (
-        <div className="border p-4 rounded shadow-md">
-            <h3 className="text-xl font-bold">{category}</h3>
-            <p className="text-gray-700">{description}</p>
+        <div className="relative border p-4 rounded shadow-md group hover:bg-gray-500 transition">
+            <button
+                onClick={() => onEdit({ id, title, category, description })}
+                className="absolute bottom-2 left-2 text-gray-500 hover:text-red-500 opacity-0 group-hover:opacity-100 transition"
+            >
+                ✏️
+            </button>
+            <button
+                onClick={() => onDelete(id)}
+                className="absolute bottom-2 right-2 text-gray-500 hover:text-red-500 opacity-0 group-hover:opacity-100 transition"
+            >
+                🗑️
+            </button>
+            <h3 className="text-xl font-bold">{title}</h3>
+            <p>Category: {category}</p>
+            <p className="text-gray-700 mb-4">{description}</p>
         </div>
     );
 }
