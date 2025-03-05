305c10e925bc4e2907a95c475017a05957abd650
Navist <zachstheman2@hotmail.com>
2025-03-05 15:27:52 -0500

    Started work on subscribers page. Shows individual subscription status.

--- dev-wins-frontend/src/app/subscriptions/page.tsx
+++ dev-wins-frontend/src/app/subscriptions/page.tsx
@@ -1,10 +1,45 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
+import { api } from "../utils/api";
+import { isAuthenticated } from "../utils/auth";
+import { useRouter } from "next/navigation";
+
+interface subStatus {
+    expires_at: Date;
+    user_id: number;
+    subscription_tier: string;
+    id: number;
+}
 
 const SubscribersPage = () => {
+    const [subStatus, setSubStatus] = useState("");
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            router.push("/users/login");
+            return;
+        }
+
+        const fetchSubStatus = async () => {
+            try {
+                const response = await api.get("/subscribers/read/");
+                setSubStatus(response.data);
+            } catch (error) {
+                console.error("Error fetching sub data:", error);
+            }
+        };
+        fetchSubStatus();
+    }, []);
+
     return (
         <div>
             <NavBar />
+            <div className="relative text-center border p-4 rounded shadow-md group hover:bg-gray-500 transition">
+                <h2>Your Subscription Tier</h2>
+                <p>{subStatus.subscription_tier}</p>
+            </div>
         </div>
     );
 };
