c39dd7423be486deda84040021224805bc2799f3
Navist <zachstheman2@hotmail.com>
2025-03-06 16:51:56 -0500

    Moved bar to the left side. Added some animation. Still playing with UI/UX

--- dev-wins-frontend/src/app/components/NavBar.tsx
+++ dev-wins-frontend/src/app/components/NavBar.tsx
@@ -7,22 +7,22 @@ export default function NavBar() {
     const router = useRouter();
 
     return (
-        <nav className="flex gap-4 p-4 bg-gray-800 text-white">
+        <nav className="absolute top-0 left-0 flex flex-col gap-4 p-4 bg-gray-800 text-white h-screen w-40">
             <button
                 onClick={() => router.push("/wins")}
-                className="px-4 py-2 bg-blue-500 rounded"
+                className="px-4 py-2 bg-blue-500 hover:bg-blue-800 rounded w-full cursor-pointer"
             >
                 View Wins
             </button>
             <button
                 onClick={() => router.push("/categories")}
-                className="px-4 py-2 bg-green-500 rounded"
+                className="px-4 py-2 bg-green-800 hover:bg-cyan-600 hover:w-40 rounded w-full cursor-pointer duration-100"
             >
                 Manage Categories
             </button>
             <button
                 onClick={() => router.push("/subscriptions")}
-                className="px-4 py-2 bg-yellow-500 rounded"
+                className="px-4 py-2 bg-green-800 hover:bg-cyan-600 hover:w-40 rounded w-full cursor-pointer duration-100"
             >
                 View Subscription
             </button>
@@ -31,7 +31,7 @@ export default function NavBar() {
                     logout();
                     router.push("/users/login");
                 }}
-                className="px-4 py-2 bg-red-500 rounded"
+                className="absolute bottom-10 left-3 px-4 py-2 bg-red-500 hover:bg-red-900 rounded w-32 cursor-pointer"
             >
                 Logout
             </button>
