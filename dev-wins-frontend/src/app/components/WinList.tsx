eb3eee6a8cc87d2ba58f0c76a2b0e166138b7ae7
Navist <zachstheman2@hotmail.com>
2025-03-09 20:52:35 -0400

    Moved loading animation to it's own file for modularity.

--- dev-wins-frontend/src/app/components/WinList.tsx
+++ dev-wins-frontend/src/app/components/WinList.tsx
@@ -1,6 +1,7 @@
 "use client";
 import WinCard from "../components/WinCard";
 import { useRouter } from "next/navigation";
+import LoadingTest from "../loadingtest/page";
 
 interface Win {
     id: number;
@@ -25,20 +26,7 @@ export default function WinList({
     const router = useRouter();
 
     if (loading) {
-        return (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {Array.from({ length: 6 }).map((_, i) => (
-                    <div
-                        key={i}
-                        className="border p-4 rounded shadow-md animate-pulse"
-                    >
-                        <div className="bg-gray-300 h-6 w-3/4 mb-2 rounded"></div>
-                        <div className="bg-gray-300 h-4 w-full mb-2 rounded"></div>
-                        <div className="bg-gray-300 h-4 w-1/2 rounded"></div>
-                    </div>
-                ))}
-            </div>
-        );
+        return <LoadingTest />;
     }
 
     return (
