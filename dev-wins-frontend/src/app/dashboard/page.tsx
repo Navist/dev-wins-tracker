2f45ac8f8f01c0cc93cfa72ce1a0fe00026dbf31
Navist <zachstheman2@hotmail.com>
2025-03-10 21:51:27 -0400

    Removed local auth check

--- dev-wins-frontend/src/app/dashboard/page.tsx
+++ dev-wins-frontend/src/app/dashboard/page.tsx
@@ -3,8 +3,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { api } from "../utils/api";
-import { isAuthenticated, logout } from "../utils/auth";
-import NavBar from "../components/NavBar";
 
 export default function DashboardPage() {
     const [user, setUser] = useState<{
@@ -14,25 +12,18 @@ export default function DashboardPage() {
     } | null>(null);
     const router = useRouter();
 
-    useEffect(() => {
-        if (!isAuthenticated()) {
-            router.push("/users/login");
-            return;
+    const fetchUser = async () => {
+        try {
+            const response = await api.get("users/get/");
+            setUser(response.data);
+        } catch (error) {
+            console.error("Error fetching user:", error);
+            // logout();
+            // router.push("/users/login");
         }
+    };
 
-        const fetchUser = async () => {
-            try {
-                const response = await api.get("users/get/");
-                setUser(response.data);
-            } catch (error) {
-                console.error("Error fetching user:", error);
-                logout();
-                router.push("/users/login");
-            }
-        };
-
-        fetchUser();
-    }, []);
+    fetchUser();
 
     return (
         <div className="min-h-screen">
