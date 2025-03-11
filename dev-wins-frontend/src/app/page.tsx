899a01426687e6488f125efa5268456cb95bdfa1
Navist <zachstheman2@hotmail.com>
2025-03-10 21:49:13 -0400

    Minor adjustments

--- dev-wins-frontend/src/app/page.tsx
+++ dev-wins-frontend/src/app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const HomePage = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("users/login/");
+    });
+
     return <div>HomePage</div>;
 };
 
