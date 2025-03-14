e3a36493a8462460f54cf38afa86be8e873e2a47
Navist <zachstheman2@hotmail.com>
2025-03-14 17:50:31 -0400

    Added auth check to homepage

--- dev-wins-frontend/src/app/page.tsx
+++ dev-wins-frontend/src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { isAuthenticated } from "./utils/auth";
 
 const HomePage = () => {
     const router = useRouter();
 
-    router.push("/dashboard");
+    if (!isAuthenticated()) {
+        router.push("/login");
+    } else {
+        router.push("/dashboard");
+    }
 
     return <div>Page of Homes</div>;
 };
